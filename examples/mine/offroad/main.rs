990f291b38e9dfa0e5715517947ee9e3f3f8a2f0
Pierre Gueth <gueth@adobe.com>
2025-02-11 15:21:28 +0100

    factor

--- examples/mine/offroad/main.rs
+++ examples/mine/offroad/main.rs
@@ -7,7 +7,6 @@ use bevy::prelude::*;
 use bevy::color::palettes::basic::SILVER;
 use bevy::color::palettes::basic::WHITE;
 use bevy::math::Affine2;
-use bevy::math::NormedVectorSpace;
 use bevy::pbr::wireframe::WireframeConfig;
 use bevy::pbr::wireframe::WireframePlugin;
 use bevy::pbr::DirectionalLightShadowMap;
@@ -15,15 +14,14 @@ use bevy::render::camera::ScalingMode;
 
 use bevy::image::{ImageAddressMode, ImageLoaderSettings, ImageSampler, ImageSamplerDescriptor};
 
-use bevy::render::mesh::Indices;
-use bevy::render::render_asset::RenderAssetUsages;
 use bevy::render::render_resource::Extent3d;
-use bevy::render::render_resource::PrimitiveTopology;
 use bevy::render::render_resource::TextureDimension;
 use bevy::render::render_resource::TextureFormat;
 // use bevy::render::mesh;
 
-use std::f32::consts::PI;
+mod track;
+
+use track::{make_road_mesh, CornerData, RoadPiece, StraightData};
 
 fn main() {
     let mut app = App::new();
@@ -185,6 +183,8 @@ fn setup(
 
 /// Creates a colorful test pattern
 fn make_uv_debug_texture() -> Image {
+    use bevy::render::render_asset::RenderAssetUsages;
+
     const TEXTURE_SIZE: usize = 8;
 
     let mut palette: [u8; 32] = [
@@ -212,186 +212,13 @@ fn make_uv_debug_texture() -> Image {
     )
 }
 
-#[derive(Debug)]
-enum RoadPiece {
-    Start,
-    Straight(StraightData),
-    Corner(CornerData),
-    Finish,
-}
-
-#[derive(Debug)]
-struct StraightData {
-    left: f32,
-    right: f32,
-    length: f32,
-}
-
-impl Default for StraightData {
-    fn default() -> Self {
-        Self {
-            left: -1.0,
-            right: 1.0,
-            length: 2.0,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct CornerData {
-    left: f32,
-    right: f32,
-    radius: f32,
-    angle: f32,
-    num_quads: u32,
-}
-
-impl CornerData {
-    fn right_turn() -> Self {
-        Self {
-            left: -1.0,
-            right: 1.0,
-            radius: 2.0,
-            angle: PI / 2.0,
-            num_quads: 8,
-        }
-    }
-    fn left_turn() -> Self {
-        Self {
-            left: -1.0,
-            right: 1.0,
-            radius: -2.0,
-            angle: PI / 2.0,
-            num_quads: 8,
-        }
-    }
-}
-
-fn make_road_mesh(pieces: &Vec<RoadPiece>) -> Mesh {
-    let initial_position = Vec3::new(-10.0, 0.25, 0.0);
-    let initial_forward = Vec3::Z;
-    let initial_up = Vec3::Y;
-
-    let mut mesh_positions: Vec<Vec3> = vec![];
-    let mut mesh_normals: Vec<Vec3> = vec![];
-    let mut mesh_triangles: Vec<u32> = vec![];
-    let mut mesh_uvs: Vec<Vec2> = vec![];
-
-    let mut push_section =
-        |position: &Vec3, forward: &Vec3, left: f32, right: f32, length: f32| -> u32 {
-            let left_pos = position + forward.cross(initial_up) * left;
-            let right_pos = position + forward.cross(initial_up) * right;
-            let next_vertex = mesh_positions.len() as u32;
-            assert!(next_vertex % 2 == 0);
-            mesh_positions.push(left_pos);
-            mesh_positions.push(right_pos);
-            mesh_normals.push(initial_up);
-            mesh_normals.push(initial_up);
-            mesh_uvs.push(Vec2::new(left, length));
-            mesh_uvs.push(Vec2::new(right, length));
-            if next_vertex >= 2 {
-                let mut tri_aa = vec![next_vertex - 2, next_vertex - 1, next_vertex];
-                let mut tri_bb = vec![next_vertex - 1, next_vertex + 1, next_vertex];
-                mesh_triangles.append(&mut tri_aa);
-                mesh_triangles.append(&mut tri_bb);
-            }
-            next_vertex
-        };
-
-    let mut current_position = initial_position.clone();
-    let mut current_forward = initial_forward.clone();
-    // let current_up = initial_up.clone();
-    let mut current_length: f32 = 0.0;
-
-    for piece in pieces {
-        match piece {
-            RoadPiece::Start => {
-                debug!("Start {:?}", current_position.clone());
-                assert!(current_length == 0.0);
-                let foo = push_section(
-                    &current_position,
-                    &current_forward,
-                    -1.0,
-                    1.0,
-                    current_length,
-                );
-                assert!(foo == 0);
-            }
-            RoadPiece::Straight(data) => {
-                debug!("Straight {:?} {:?}", current_position.clone(), data);
-                current_position += current_forward * data.length;
-                current_length += data.length;
-                assert!(current_length != 0.0);
-                let foo = push_section(
-                    &current_position,
-                    &current_forward,
-                    data.left,
-                    data.right,
-                    current_length,
-                );
-                assert!(foo > 0);
-            }
-            RoadPiece::Corner(data) => {
-                debug!("Corner {:?} {:?}", current_position.clone(), data);
-                assert!(data.num_quads > 0);
-                let current_right = current_forward.cross(initial_up);
-                let center = current_position + current_right * data.radius;
-                let sign: f32 = if data.radius < 0.0 { 1.0 } else { -1.0 };
-                for kk in 0..data.num_quads {
-                    let ang = (kk + 1) as f32 / data.num_quads as f32 * data.angle;
-                    let pos = center - current_right * data.radius * f32::cos(ang)
-                        + current_forward * f32::abs(data.radius) * f32::sin(ang);
-                    let fwd = Quat::from_axis_angle(initial_up, sign * ang) * current_forward;
-                    let len = f32::abs(data.radius) * ang + current_length;
-                    let foo = push_section(&pos, &fwd, data.left, data.right, len);
-                    assert!(foo > 0);
-                }
-                current_position += current_forward * f32::abs(data.radius);
-                current_forward =
-                    Quat::from_axis_angle(initial_up, sign * data.angle) * current_forward;
-                current_position += current_forward * data.radius;
-                current_length += f32::abs(data.radius) * data.angle;
-            }
-            RoadPiece::Finish => {
-                let pos_error = (current_position - initial_position).norm();
-                let dir_error = (current_forward - initial_forward).norm();
-                let is_looping: bool = pos_error < 1e-3 && dir_error < 1e-3;
-                debug!(
-                    "Finish {:?} pos_err {:0.3e} dir_err {:0.3e} total_length {} loop {}",
-                    current_position.clone(),
-                    pos_error,
-                    dir_error,
-                    current_length,
-                    is_looping,
-                );
-                if !is_looping {
-                    warn!("!!! road is not looping !!!");
-                }
-            }
-        }
-        //     push_road(piece);
-    }
-
-    assert!(mesh_triangles.len() % 3 == 0);
-    info!("num_vertices {}", mesh_positions.len());
-    info!("num_triangles {}", mesh_triangles.len() / 3);
-    info!("total_length {}", current_length);
-
-    let mut mesh = Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
-    );
-
-    mesh = mesh.with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, mesh_positions);
-    mesh = mesh.with_inserted_indices(Indices::U32(mesh_triangles));
-    mesh = mesh.with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, mesh_normals);
-    mesh = mesh.with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, mesh_uvs);
-
-    mesh
-}
-
 #[rustfmt::skip]
 fn make_cube_mesh() -> Mesh {
+    use bevy::render::mesh::Indices;
+    use bevy::render::mesh::Mesh;
+    use bevy::render::render_asset::RenderAssetUsages;
+    use bevy::render::render_resource::PrimitiveTopology;
+
     // Keep the mesh data accessible in future frames to be able to mutate it in toggle_texture.
     Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD)
     .with_inserted_attribute(
