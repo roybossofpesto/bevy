a81787a9fd81d7869d52cc95cf5329cf57ae1243
Pierre Gueth <gueth@adobe.com>
2025-02-11 23:11:24 +0100

    factor out

--- examples/mine/offroad/main.rs
+++ examples/mine/offroad/main.rs
@@ -4,11 +4,9 @@ use bevy::prelude::*;
 
 // use bevy::color::palettes::basic::RED;
 use bevy::color::palettes::basic::BLUE;
-use bevy::color::palettes::basic::SILVER;
 use bevy::math::Affine2;
 use bevy::pbr::DirectionalLightShadowMap;
 use bevy::pbr::UvChannel;
-use bevy::render::camera::ScalingMode;
 
 use bevy::image::{ImageAddressMode, ImageLoaderSettings, ImageSampler, ImageSamplerDescriptor};
 
@@ -17,7 +15,7 @@ use std::f32::consts::PI;
 mod scene;
 mod track;
 
-use scene::{make_cube_mesh, make_uv_debug_texture};
+use scene::{populate_background, populate_camera_and_lights};
 use track::{make_track_mesh, TRACK0_DATA, TRACK1_DATA};
 
 fn main() {
@@ -60,6 +58,8 @@ fn main() {
     }
 
     app.add_systems(Startup, setup);
+    app.add_systems(Startup, populate_background);
+    app.add_systems(Startup, populate_camera_and_lights);
 
     app.run();
 }
@@ -67,102 +67,11 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut images: ResMut<Assets<Image>>,
+    // mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
     info!("** setup **");
-
-    // ground plane
-    commands.spawn((
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10))),
-        MeshMaterial3d(materials.add(Color::from(SILVER))),
-    ));
-
-    // tower
-    let debug_material = materials.add(StandardMaterial {
-        base_color_texture: Some(images.add(make_uv_debug_texture())),
-        ..default()
-    });
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 5.0, 1.0))),
-        MeshMaterial3d(debug_material),
-        Transform::from_xyz(5.0, 3.0, 5.0),
-    ));
-
-    // cube
-    commands.spawn((
-        Mesh3d(meshes.add(make_cube_mesh())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color_texture: Some(asset_server.load("textures/array_texture.png")),
-            ..default()
-        })),
-        Transform::from_xyz(8.0, 3.0, 5.0),
-    ));
-
-    // parallal
-    let parallal_material = materials.add(StandardMaterial {
-        perceptual_roughness: 0.2,
-        normal_map_channel: UvChannel::Uv1,
-        normal_map_texture: Some(asset_server.load_with_settings(
-            "textures/parallax_example/cube_normal.png",
-            // The normal map texture is in linear color space. Lighting won't look correct
-            // if `is_srgb` is `true`, which is the default.
-            |settings: &mut ImageLoaderSettings| {
-                *settings = ImageLoaderSettings {
-                    is_srgb: false,
-                    sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
-                        address_mode_u: ImageAddressMode::Repeat,
-                        address_mode_v: ImageAddressMode::Repeat,
-                        ..default()
-                    }),
-                    ..default()
-                }
-            },
-        )),
-        base_color_channel: UvChannel::Uv1,
-        base_color_texture: Some(asset_server.load_with_settings(
-            "textures/parallax_example/cube_color.png",
-            |settings: &mut ImageLoaderSettings| {
-                *settings = ImageLoaderSettings {
-                    sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
-                        address_mode_u: ImageAddressMode::Repeat,
-                        address_mode_v: ImageAddressMode::Repeat,
-                        ..default()
-                    }),
-                    ..default()
-                }
-            },
-        )),
-        depth_map: Some(asset_server.load_with_settings(
-            "textures/parallax_example/cube_depth.png",
-            |settings: &mut ImageLoaderSettings| {
-                *settings = ImageLoaderSettings {
-                    sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
-                        address_mode_u: ImageAddressMode::Repeat,
-                        address_mode_v: ImageAddressMode::Repeat,
-                        ..default()
-                    }),
-                    ..default()
-                }
-            },
-        )),
-        parallax_depth_scale: 0.1,
-        max_parallax_layer_count: 32.0,
-        parallax_mapping_method: ParallaxMappingMethod::Relief { max_steps: 16 },
-        ..default()
-    });
-    commands.spawn((
-        Mesh3d(
-            meshes.add(
-                Mesh::from(Cuboid::default())
-                    .with_generated_tangents()
-                    .unwrap(),
-            ),
-        ),
-        MeshMaterial3d(parallal_material),
-        Transform::from_xyz(3.0, 2.0, 18.0).with_scale(Vec3::ONE * 4.0),
-    ));
 
     // track 0 showcases flow parametrization
     let track0_material = materials.add(StandardMaterial {
@@ -210,7 +119,7 @@ fn setup(
     commands.spawn((
         Mesh3d(meshes.add(make_track_mesh(&TRACK1_DATA))),
         MeshMaterial3d(track1_material),
-        Transform::from_xyz(-1.0, 0.0, -2.0)
+        Transform::from_xyz(-1.0, 0.0, -2.0),
     ));
 
     // track2 showcases water effect
@@ -225,37 +134,4 @@ fn setup(
     ));
 
     // "textures/BlueNoise-Normal.png",
-
-    // lights
-    commands.spawn((
-        PointLight {
-            shadows_enabled: true,
-            intensity: 1.0e6,
-            range: 100.0,
-            shadow_depth_bias: 0.2,
-            ..default()
-        },
-        Transform::from_xyz(-4.0, 16.0, 8.0),
-    ));
-    commands.spawn((
-        DirectionalLight {
-            color: Color::WHITE,
-            shadows_enabled: true,
-            illuminance: light_consts::lux::CLEAR_SUNRISE,
-            ..default()
-        },
-        Transform::from_translation(Vec3::Y).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
-
-    commands.spawn((
-        Camera3d::default(),
-        Projection::from(OrthographicProjection {
-            // 20 world units per pixel of window height.
-            scaling_mode: ScalingMode::FixedVertical {
-                viewport_height: 20.0,
-            },
-            ..OrthographicProjection::default_3d()
-        }),
-        Transform::from_xyz(-10.0, 10.0, 14.0).looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
-    ));
 }
--- examples/mine/offroad/scene.rs
+++ examples/mine/offroad/scene.rs
@@ -1,5 +1,169 @@
+use bevy::prelude::{Assets, Commands, Res, ResMut};
+
+use bevy::color::palettes::basic::SILVER;
+
+pub fn populate_background(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<bevy::render::mesh::Mesh>>,
+    mut images: ResMut<Assets<bevy::image::Image>>,
+    mut materials: ResMut<Assets<bevy::pbr::StandardMaterial>>,
+    asset_server: Res<bevy::asset::AssetServer>,
+) {
+    use bevy::prelude::*;
+
+    info!("** populate_background **");
+
+    // ground plane
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10))),
+        MeshMaterial3d(materials.add(Color::from(SILVER))),
+    ));
+
+    // tower
+    let debug_material = materials.add(StandardMaterial {
+        base_color_texture: Some(images.add(make_uv_debug_texture())),
+        ..default()
+    });
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 5.0, 1.0))),
+        MeshMaterial3d(debug_material),
+        Transform::from_xyz(5.0, 3.0, 5.0),
+    ));
+
+    // cube
+    commands.spawn((
+        Mesh3d(meshes.add(make_cube_mesh())),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color_texture: Some(asset_server.load("textures/array_texture.png")),
+            ..default()
+        })),
+        Transform::from_xyz(8.0, 3.0, 5.0),
+    ));
+
+    // parallal
+    let parallal_material = materials.add(make_parallax_material(asset_server));
+    commands.spawn((
+        Mesh3d(
+            meshes.add(
+                Mesh::from(Cuboid::default())
+                    .with_generated_tangents()
+                    .unwrap(),
+            ),
+        ),
+        MeshMaterial3d(parallal_material),
+        Transform::from_xyz(3.0, 2.0, 18.0).with_scale(Vec3::ONE * 4.0),
+    ));
+}
+
+pub fn populate_camera_and_lights(mut commands: Commands) {
+    use bevy::prelude::*;
+    use bevy::render::camera::ScalingMode;
+
+    info!("** populate_camera_and_lights **");
+
+    // lights
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            intensity: 1.0e6,
+            range: 100.0,
+            shadow_depth_bias: 0.2,
+            ..default()
+        },
+        Transform::from_xyz(-4.0, 16.0, 8.0),
+    ));
+    commands.spawn((
+        DirectionalLight {
+            color: Color::WHITE,
+            shadows_enabled: true,
+            illuminance: light_consts::lux::CLEAR_SUNRISE,
+            ..default()
+        },
+        Transform::from_translation(Vec3::Y).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Projection::from(OrthographicProjection {
+            // 20 world units per pixel of window height.
+            scaling_mode: ScalingMode::FixedVertical {
+                viewport_height: 20.0,
+            },
+            ..OrthographicProjection::default_3d()
+        }),
+        Transform::from_xyz(-10.0, 10.0, 14.0).looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
+    ));
+}
+
+fn make_parallax_material(
+    // mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<bevy::asset::AssetServer>,
+) -> bevy::pbr::StandardMaterial {
+    use bevy::image::ImageAddressMode;
+    use bevy::image::ImageLoaderSettings;
+    use bevy::image::ImageSampler;
+    use bevy::image::ImageSamplerDescriptor;
+    use bevy::math::Affine2;
+    use bevy::math::Vec2;
+    use bevy::pbr::UvChannel;
+    use bevy::prelude::ParallaxMappingMethod;
+    bevy::pbr::StandardMaterial {
+        perceptual_roughness: 0.2,
+        normal_map_channel: UvChannel::Uv1,
+        normal_map_texture: Some(asset_server.load_with_settings(
+            "textures/parallax_example/cube_normal.png",
+            // The normal map texture is in linear color space. Lighting won't look correct
+            // if `is_srgb` is `true`, which is the default.
+            |settings: &mut ImageLoaderSettings| {
+                *settings = ImageLoaderSettings {
+                    is_srgb: false,
+                    sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
+                        address_mode_u: ImageAddressMode::Repeat,
+                        address_mode_v: ImageAddressMode::Repeat,
+                        ..ImageSamplerDescriptor::default()
+                    }),
+                    ..ImageLoaderSettings::default()
+                }
+            },
+        )),
+        base_color_channel: UvChannel::Uv1,
+        base_color_texture: Some(asset_server.load_with_settings(
+            "textures/parallax_example/cube_color.png",
+            |settings: &mut ImageLoaderSettings| {
+                *settings = ImageLoaderSettings {
+                    sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
+                        address_mode_u: ImageAddressMode::Repeat,
+                        address_mode_v: ImageAddressMode::Repeat,
+                        ..ImageSamplerDescriptor::default()
+                    }),
+                    ..ImageLoaderSettings::default()
+                }
+            },
+        )),
+        depth_map: Some(asset_server.load_with_settings(
+            "textures/parallax_example/cube_depth.png",
+            |settings: &mut ImageLoaderSettings| {
+                *settings = ImageLoaderSettings {
+                    sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
+                        address_mode_u: ImageAddressMode::Repeat,
+                        address_mode_v: ImageAddressMode::Repeat,
+                        ..ImageSamplerDescriptor::default()
+                    }),
+                    ..ImageLoaderSettings::default()
+                }
+            },
+        )),
+        parallax_depth_scale: 0.1,
+        max_parallax_layer_count: 32.0,
+        parallax_mapping_method: ParallaxMappingMethod::Relief { max_steps: 16 },
+        uv_transform: Affine2::from_scale(Vec2::ONE * 2.0),
+        ..bevy::pbr::StandardMaterial::default()
+    }
+}
+
 /// Creates a colorful test pattern
-pub fn make_uv_debug_texture() -> bevy::image::Image {
+fn make_uv_debug_texture() -> bevy::image::Image {
     use bevy::render::render_asset::RenderAssetUsages;
     use bevy::render::render_resource::Extent3d;
     use bevy::render::render_resource::TextureDimension;
@@ -33,7 +197,7 @@ pub fn make_uv_debug_texture() -> bevy::image::Image {
 }
 
 #[rustfmt::skip]
-pub fn make_cube_mesh() -> bevy::render::mesh::Mesh {
+fn make_cube_mesh() -> bevy::render::mesh::Mesh {
     use bevy::render::mesh::Indices;
     use bevy::render::mesh::Mesh;
     use bevy::render::render_asset::RenderAssetUsages;
