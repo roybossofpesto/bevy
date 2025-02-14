5376e6ef730c7081946824bc65434988d55c9e17
Pierre Gueth <gueth@adobe.com>
2025-02-14 18:30:34 +0100

    update_vehicule_physics

--- examples/mine/offroad/vehicule.rs
+++ examples/mine/offroad/vehicule.rs
@@ -12,6 +12,7 @@ impl Plugin for VehiculePlugin {
         info!("** build_vehicule **");
 
         app.add_systems(Startup, setup_vehicule);
+        app.add_systems(Update, update_vehicule_physics);
     }
     // fn finish(&self, app: &mut App) {
     //     info!("** simu_finish **");
@@ -21,6 +22,18 @@ impl Plugin for VehiculePlugin {
 }
 
 //////////////////////////////////////////////////////////////////////
+
+enum Player {
+    One,
+    Two,
+}
+
+#[derive(Component)]
+struct BoatData {
+    player: Player,
+    pos_prev: [f32; 2],
+    pos_current: [f32; 2],
+}
 
 fn setup_vehicule(
     mut commands: Commands,
@@ -38,12 +51,18 @@ fn setup_vehicule(
 
     let red_pos = Vec3::new(-11.5, 0.0, 0.0);
     let blue_pos = Vec3::new(-12.5, 0.0, 0.0);
+
     commands.spawn((
         Mesh3d(my_mesh.clone()),
         MeshMaterial3d(materials.add(Color::from(YELLOW))),
         Transform::from_translation(red_pos)
             .looking_at(red_pos + Vec3::Z, Vec3::Y)
             .with_scale(Vec3::ONE * 0.15),
+        BoatData {
+            player: Player::One,
+            pos_prev: red_pos.xz().into(),
+            pos_current: red_pos.xz().into(),
+        },
     ));
     commands.spawn((
         Mesh3d(my_mesh),
@@ -51,5 +70,55 @@ fn setup_vehicule(
         Transform::from_translation(blue_pos)
             .looking_at(blue_pos + Vec3::Z, Vec3::Y)
             .with_scale(Vec3::ONE * 0.15),
+        BoatData {
+            player: Player::Two,
+            pos_prev: blue_pos.xz().into(),
+            pos_current: blue_pos.xz().into(),
+        },
     ));
+}
+
+fn update_vehicule_physics(
+    mut query: Query<(&mut BoatData, &mut Transform)>,
+    // time: Res<Time>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    for (mut data, mut transform) in &mut query {
+        let mut pos_next = data.pos_current;
+        match data.player {
+            Player::One => {
+                if keyboard.just_pressed(KeyCode::Enter) {
+                    pos_next[1] += 1.0;
+                }
+                // if keyboard.just_pressed(KeyCode::ArrowRight) {
+                //     delta.x += 1.0;
+                // }
+                // if keyboard.just_pressed(KeyCode::ArrowUp) {
+                //     delta.y += 1.0;
+                // }
+                // if keyboard.just_pressed(KeyCode::ArrowDown) {
+                //     delta.y -= 1.0;
+                // }
+                // transform.translation += delta;
+            }
+            Player::Two => {
+                if keyboard.just_pressed(KeyCode::KeyA) {
+                    pos_next[0] += 1.0;
+                }
+                // if keyboard.just_pressed(KeyCode::ArrowRight) {
+                //     delta.x += 1.0;
+                // }
+                // if keyboard.just_pressed(KeyCode::ArrowUp) {
+                //     delta.y += 1.0;
+                // }
+                // if keyboard.just_pressed(KeyCode::ArrowDown) {
+                //     delta.y -= 1.0;
+                // }
+                // transform.translation += delta;
+            }
+        }
+        transform.translation = Vec3::new(pos_next[0], 0.0, pos_next[1]);
+        data.pos_prev = data.pos_current;
+        data.pos_current = pos_next;
+    }
 }
