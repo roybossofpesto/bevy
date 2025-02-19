c811c58b2edff69eaed6c4cacfeaeb613b60b4a7
Pierre Gueth <gueth@adobe.com>
2025-02-19 12:13:18 +0100

    should compat with wasm

--- examples/mine/morpheus_viewer/morpheus.rs
+++ examples/mine/morpheus_viewer/morpheus.rs
@@ -18,16 +18,18 @@ impl Plugin for MorpheusPlugin {
     }
 }
 
+//////////////////////////////////////////////////////////////////////
+
 fn populate_models(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
+    mut images: ResMut<Assets<Image>>,
 ) {
     commands.spawn((
         Mesh3d(meshes.add(Mesh::from(Cuboid::new(2.0, 2.0, 2.0)))),
         MeshMaterial3d(materials.add(StandardMaterial {
-            base_color_texture: Some(asset_server.load("textures/array_texture.png")),
+            base_color_texture: Some(images.add(make_texture())),
             ..default()
         })),
         Transform::from_xyz(0.0, 0.0, 0.0),
@@ -103,4 +105,53 @@ fn animate_camera(
     if keyboard_input.just_pressed(KeyCode::KeyR) {
         transform.rotation = Quat::IDENTITY;
     }
+}
+
+//////////////////////////////////////////////////////////////////////
+
+fn make_texture() -> Image {
+    use bevy::image::ImageSampler;
+    use bevy::render::render_asset::RenderAssetUsages;
+    use bevy::render::render_resource::Extent3d;
+    use bevy::render::render_resource::TextureDimension;
+    use bevy::render::render_resource::TextureFormat;
+
+    const TEXTURE_SIZE: usize = 8;
+
+    // let mut palette: [u8; 32] = [
+    //     255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
+    //     198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
+    // ];
+
+    let uu_color: [u8; 4] = [255, 0, 0, 255];
+    let vv_color: [u8; 4] = [0, 255, 0, 255];
+
+    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+    texture_data.fill(128);
+
+    for ii in 0..TEXTURE_SIZE {
+        let offset = ii * 4;
+        texture_data[offset..(offset + 4)].copy_from_slice(&uu_color);
+    }
+
+    for jj in 0..TEXTURE_SIZE {
+        let offset = TEXTURE_SIZE * jj * 4;
+        texture_data[offset..(offset + 4)].copy_from_slice(&vv_color);
+    }
+
+    let mut image = Image::new_fill(
+        Extent3d {
+            width: TEXTURE_SIZE as u32,
+            height: TEXTURE_SIZE as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &texture_data,
+        TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+
+    image.sampler = ImageSampler::nearest();
+
+    image
 }
